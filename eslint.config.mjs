a0cb13d586debf53e24fd55666eb904001fafbab
apades <36807043+apades@users.noreply.github.com>
2024-08-24 12:52:50 +0800

    fix: eslint config + git hook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,13 @@
 import antfu from '@antfu/eslint-config'
-import hooksPlugin from 'eslint-plugin-react-hooks'
 
 export default antfu(
   {
-    plugins: {
-      'react-hooks': hooksPlugin,
-    },
     react: true,
     rules: {
       'node/prefer-global/process': 'off',
       'ts/no-require-imports': 'off',
       'react/prefer-destructuring-assignment': 'off',
+      'react-hooks/exhaustive-deps': 'off',
     },
   },
 )
